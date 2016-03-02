bea553f42493185e2a1d346860b7615f93a8248e
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2016-03-02 15:46:22 +0100

    #! prepare for ima version 12

---
+++ src/__tests__/mainSpec.js
@@ -0,0 +1,15 @@
+import * as Main from '../main';
+
+describe('Main', () => {
+	it('should export $registerImaPlugin', () => {
+		expect(typeof Main.$registerImaPlugin).toEqual('function');
+	});
+
+	it('should export GoogleAnalytic', () => {
+		expect(typeof Main.GoogleAnalytic).toEqual('function');
+	});
+
+	it('should export defaultDependencies', () => {
+		expect(Array.isArray(Main.defaultDependencies)).toEqual(true);
+	});
+});
