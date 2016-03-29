d32a0429154a54135fc9511c24b780ce71bb3893
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2016-03-29 16:07:24 +0200

    #! build new dist files

---
+++ dist/Events.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+const Events = Object.freeze({
+	LOADED: 'ima.plugin.analytic.loaded'
+});
+
+exports.default = Events;
