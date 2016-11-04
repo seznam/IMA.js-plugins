69da71b9e603ae8122500f794a9d0f23da229983
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-11-04 14:34:04 +0100

    switched to gulp 4, improved the build & publish process

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,49 @@ require('babel-core/register.js')({
 	'plugins': 'transform-es2015-modules-commonjs'
 });
 
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
-var jasmine = require('gulp-jasmine');
+let del = require('del');
+let gulp = require('gulp');
+let sourcemaps = require('gulp-sourcemaps');
+let babel = require('gulp-babel');
+let jasmine = require('gulp-jasmine');
 
-// build module
-gulp.task('build', function() {
-	return (
-		gulp.src('./src/**/!(*Spec).js')
+exports.build = gulp.series(
+	clean,
+	gulp.parallel(
+		copy,
+		build_js
+	)
+);
+
+function build_js() {
+	return gulp
+		.src('./src/**/!(*Spec).js')
 		.pipe(sourcemaps.init())
 		.pipe(babel({
 			moduleIds: true,
 			plugins: ['transform-es2015-modules-commonjs']
 		}))
-		.pipe(gulp.dest('./dist'))
-	);
-});
+		.pipe(gulp.dest('./dist'));
+}
 
-//run test
-gulp.task('test', () => {
-	return (
-		gulp.src('./src/**/*Spec.js')
-			.pipe(jasmine())
-	);
-});
+function copy() {
+	return gulp
+		.src(['./package.json', './LICENSE', './README.md'])
+		.pipe(gulp.dest('./dist'));
+}
 
-// -------------------------------------PRIVATE HELPER TASKS
-gulp.task('dev', function() {
-	gulp.watch(['./src/**/*.js'], ['test']);
-});
+function clean() {
+	return del('./dist');
+}
+
+exports.test = test;
+function test() {
+	return gulp
+		.src('./src/**/*Spec.js')
+		.pipe(jasmine());
+}
+
+exports.dev = dev;
+function dev() {
+	return gulp.watch(['./src/**/*.js'], test);
+}
