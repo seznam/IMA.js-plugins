24febcb248b556be5956bd70552cbd1e66bbcd78
Ondřej Baše <ondrej.base@firma.seznam.cz>
2017-06-13 08:49:40 +0200

    # Main script and fixes

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import StyleLoaderPlugin from './StyleLoaderPlugin';
+import Events from './Events';
+
+let defaultDependencies = StyleLoaderPlugin.$dependencies;
+
+let $registerImaPlugin = (ns) => {
+};
+
+export {
+	StyleLoaderPlugin,
+	Events,
+	defaultDependencies,
+	$registerImaPlugin
+};
