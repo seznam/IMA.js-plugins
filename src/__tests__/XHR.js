a1496d6aef2587214b040e185217650d8baefdde
Martin Jurča <martin.jurca@firma.seznam.cz>
2017-09-24 20:09:02 +0200

    added mocks for non-serialized global objects and an XMLHttpRequest mock

--- src/__tests__/XHR.js
+++ src/__tests__/XHR.js
@@ -6,11 +6,123 @@ describe('XHR', () => {
 			return true;
 		}
 	};
+	class Blob {}
+	class BufferSource {}
+	class FormData {}
+	class URLSearchParams {}
+	class ReadableStream {}
 
-	let xhr;
+	let pluginInstance;
+	let xhrSendCallback = () => Promise.resolve(null);
+	let xhrProgressInterval = 10; // milliseconds
+	let xhrResponseHeaders = [['content-type', 'application/javascript']];
+
+	beforeAll(() => {
+		global.Blob = global.Blob || Blob;
+		global.BufferSource = global.BufferSource || BufferSource;
+		global.FormData = global.FormData || FormData;
+		global.URLSearchParams = global.URLSearchParams || URLSearchParams;
+		global.ReadableStream = global.ReadableStream || ReadableStream;
+
+		global.XMLHttpRequest = class XMLHttpRequest {
+			constructor() {
+				this.readyState = 0;
+				this.status = 0;
+				this.response = undefined;
+
+				this._listeners = {};
+				this._requestHeaders = [];
+			}
+
+			open(method, url, async, username, password) {
+				if (async !== true) {
+					throw new Error(`The async flag must be true, but ${async} was provided`);
+				}
+				if (username || password) {
+					throw new Error(
+						'The HTTP Auth info (username and password) must not be provided, but ' +
+						`${username}:${password} was provided`
+					);
+				}
+				this._method = method;
+				this._url = url;
+			}
+
+			addEventListener(type, callback) {
+				if (!['readystatechange', 'progress', 'load', 'abort', 'timeout', 'error'].includes(type)) {
+					throw new Error(`Unsupported event type: ${type}`);
+				}
+				if (this._listeners[type]) {
+					throw new Error(`Cannot register more listener of the ${type} event`);
+				}
+
+				this._listeners[type] = callback;
+			}
+
+			setRequestHeader(headerName, value) {
+				this._requestHeaders.push([headerName, value]);
+			}
+
+			abort() {
+				this._aborted = true;
+				this._listeners.abort({ type: 'abort', target: this });
+			}
+
+			send(body) {
+				let progressIntervalId;
+				let nextImmediate;
+
+				nextImmediate = setImmediate(() => {
+					this.readyState = 1;
+					this._listeners.readystatechange({ type: 'readystatechange', target: this });
+
+					nextImmediate = setImmediate(() => {
+						this.readyState = 2;
+						this._listeners.readystatechange({ type: 'readystatechange', target: this });
+
+						progressIntervalId = setInterval(() => {
+							this._listeners.progress({ type: 'progress', target: this });
+						}, xhrProgressInterval);
+
+						xhrSendCallback(this, body).then(responseBody => {
+							this.readyState = 3;
+							this._listeners.readystatechange({ type: 'readystatechange', target: this });
+							clearInterval(progressIntervalId);
+							if (this._timeoutId) {
+								clearTimeout(this._timeoutId);
+							}
+
+							nextImmediate = setImmediate(() => {
+								this.response = responseBody;
+								this.readyState = 4;
+								this._listeners.readystatechange({ type: 'readystatechange', target: this });
+								this._listeners.load({ type: 'load', target: this });
+							});
+						}).catch(error => {
+							this._listeners.error({ type: 'error', target: this, error });
+						});
+					});
+				});
+
+				if (this.timeout) {
+					this._timeoutId = setTimeout(() => {
+						if (progressIntervalId) {
+							clearInterval(progressIntervalId);
+						}
+						clearImmediate(nextImmediate);
+						this._listeners.timeout({ type: 'timeout', target: this });
+					}, this.timeout);
+				}
+			}
+
+			getAllResponseHeaders() {
+				return xhrResponseHeaders.map(pair => pair.join(': ')).join('\r\n') + '\r\n';
+			}
+		};
+	});
 
 	beforeEach(() => {
-		xhr = new XHR(windowMock);
+		pluginInstance = new XHR(windowMock);
 	});
 
 	using(['get', 'post', 'put', 'patch', 'delete'], (method) => {
@@ -18,6 +130,10 @@ describe('XHR', () => {
 			const serverSideWindowMock = {
 				isClient() {
 					return false;
+				},
+
+				getWindow() {
+					return global;
 				}
 			};
 			const xhr = new XHR(serverSideWindowMock);
@@ -66,6 +182,10 @@ describe('XHR', () => {
 		it(`should use the default options as fallbacks for the provided options of a ${method} request`, () => {
 			// try with default as well headers
 		});
+	});
+
+	afterAll(() => {
+		delete global.XMLHttpRequest;
 	});
 
 	function using(values, func) {
