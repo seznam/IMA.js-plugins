d27c1231df40739efe425b384b453d3ef9153e35
Vojtěch Šimko <SimkoVojta@gmail.com>
2019-09-26 13:19:53 +0200

    plugin-logger made public

---
+++ packages/plugin-logger/README.md
@@ -0,0 +1,97 @@
+# ima-plugin-analytic-fb-pixel
+
+This is the Facebook Pixel analytic plugin for the IMA.js application. You can find the IMA.js skeleton application at <https://github.com/seznam/IMA.js-skeleton>
+or follow link <https://imajs.io>.
+
+## Installation
+
+```javascript
+
+npm install ima-plugin-analytic-fb-pixel ima-plugin-script-loader --save
+
+```
+
+```javascript
+// /app/build.js
+
+var vendors = {
+	common: [
+		'ima-plugin-analytic-fb-pixel',
+		'ima-plugin-analytic',
+		'ima-plugin-script-loader'
+	]
+};
+
+/*
+Now is FB Pixel analytic plugin available from:
+
+import { FacebookPixelAnalytic, defaultDependencies } from 'ima-plugin-analytic-fb-pixel';
+*/
+
+```
+
+```javascript
+// /app/config/settings.js
+
+prod: {
+	$Http: { ... },
+	$Cache: { ... },
+	$Page:{ ... },
+	plugin : {
+		analytic: {
+			fbPixel: {
+				id: 'XXX'
+			}
+		}
+	}
+}
+```
+
+```javascript
+// /app/config/services.js
+import { FacebookPixelAnalytic } from 'ima-plugin-analytic-fb-pixel'
+
+var $window = oc.get('$Window');
+var $dispatcher = oc.get('$Dispatcher');
+var fbPixelAnalytic = oc.get(FacebookPixelAnalytic);
+
+
+if ($window.isClient()) {
+
+	// initialize analytic
+	fbPixelAnalytic.init();
+
+	//set hit page view to analytic
+	$dispatcher.listen(ns.ima.router.Events.AFTER_HANDLE_ROUTE, (pageData) => {
+
+		if (pageData &&
+				pageData.response &&
+				(pageData.response.status >= 200 &&
+				pageData.response.status < 300)) {
+
+			fbPixelAnalytic.hitPageView(pageData);
+		}
+	});
+}
+```
+
+## Version 1.0 notice
+
+Since version 1.0 you need to additionally call a `load()` method. The later you call this method the better.
+If you don't have a specific point in your app where you know that the page has finished loading you can call the `load()` method immediatelly after `init()` method
+
+```javascript
+if ($window.isClient()) {
+
+	// insert analytic script to page and initialization analytic
+	fbPixelAnalytic.init();
+	fbPixelAnalytic.load();
+
+	// ...
+```
+
+## Dependencies
+If you are looking more details, you should
+follow this links:
+[https://github.com/seznam/IMA.js-plugin-analytic](https://github.com/seznam/IMA.js-plugin-analytic),
+[https://github.com/seznam/IMA.js-plugin-script-loader](https://github.com/seznam/IMA.js-plugin-script-loader)
---
+++ packages/plugin-logger/src/main.js
@@ -0,0 +1,279 @@
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+/**
+ * A configuration.
+ * @typedef {Object} Configuration
+ * @property {boolean} [silentMode=false] Indicates whether the logging is
+ *           silenced.
+ */
+
+/**
+ * A configuration.
+ * @type {module:main~Configuration}
+ */
+let _config = {
+  silentMode: false
+};
+
+/**
+ * Configure the logger.
+ *
+ * @param {module:main~Configuration} config Some or all of configuration
+ *        properties, which overwrites the current values.
+ */
+function configureLogger(config) {
+  if (!config || typeof config !== 'object') {
+    exports.error(new TypeError('Argument config must be an object.'));
+
+    return;
+  }
+
+  if ('silentMode' in config) {
+    _config.silentMode = !!config.silentMode;
+  }
+}
+
+/**
+ * Activates the silent mode. It's useful for unit testing.
+ *
+ * @example
+ * import * as logger from '@usa/plugin-logger';
+ *
+ * describe('some class with the logger', () => {
+ *   // Shortcut for configureLogger({ silentMode: true });
+ *   logger.beSilent();
+ * });
+ */
+function beSilent() {
+  configureLogger({ silentMode: true });
+}
+
+/**
+ * Checks if the logging is in the silent mode.
+ *
+ * @return {boolean} TRUE = when it's active, otherwise FALSE.
+ */
+function isSilent() {
+  return _config.silentMode;
+}
+
+/**
+ * Outputs a message.
+ *
+ * @param {...*} message A message.
+ */
+function log(...message) {
+  if (!exports.isSilent()) {
+		console.log(...message); // eslint-disable-line
+  }
+}
+
+/**
+ * Outputs an informational message.
+ *
+ * @param {...*} message An informational message.
+ */
+function info(...message) {
+  if (!exports.isSilent()) {
+		console.info(...message); //eslint-disable-line
+  }
+}
+
+/**
+ * Outputs a warning message.
+ *
+ * @param {...*} message A warning message.
+ */
+function warn(...message) {
+  if (!exports.isSilent()) {
+		console.warn(...message); //eslint-disable-line
+  }
+}
+
+/**
+ * Outputs an error message.
+ *
+ * @param {...*} message An error message.
+ */
+function error(...message) {
+  if (!exports.isSilent()) {
+		console.error(...message); //eslint-disable-line
+  }
+}
+
+/**
+ * Outputs a debug message.
+ *
+ * @param {...*} message A debug message.
+ */
+function debug(...message) {
+  if (!exports.isSilent()) {
+		if (typeof console.debug === 'function') { //eslint-disable-line
+			console.debug(...message); //eslint-disable-line
+    } else {
+			console.log(...message); //eslint-disable-line
+    }
+  }
+}
+
+/**
+ * An expression that is evaluated as a boolean value.
+ * @typedef {*} Condition
+ */
+
+/**
+ * Outputs a message if a condition is met.
+ *
+ * @param {module:main~Condition} condition A condition.
+ * @param {...*} message A message.
+ * @return {booolean} TRUE when the condition is met, otherwise FALSE.
+ */
+function logIf(condition, ...message) {
+  if (condition) {
+    exports.log(...message);
+  }
+
+  return !!condition;
+}
+
+/**
+ * Outputs an informational message if a condition is met.
+ *
+ * @param {module:main~Condition} condition A condition.
+ * @param {...*} message An informational message.
+ * @return {booolean} TRUE when the condition is met, otherwise FALSE.
+ */
+function infoIf(condition, ...message) {
+  if (condition) {
+    exports.info(...message);
+  }
+
+  return !!condition;
+}
+
+/**
+ * Outputs a warning message if a condition is met.
+ *
+ * @param {module:main~Condition} condition A condition.
+ * @param {...*} message A warning message.
+ * @return {booolean} TRUE when the condition is met, otherwise FALSE.
+ */
+function warnIf(condition, ...message) {
+  if (condition) {
+    exports.warn(...message);
+  }
+
+  return !!condition;
+}
+
+/**
+ * Outputs an error message if a condition is met.
+ *
+ * @example
+ * function foo(num, str) {
+ *   if (
+ *     errorIf(typeof num !== 'number', new TypeError('Argument num must be a number.')) ||
+ *     errorIf(typeof str !== 'string', new TypeError('Argument str must be a string.'))
+ *   ) {
+ *     // One or both of previous errors have been logged.
+ *     return;
+ *   }
+ *   ...
+ * }
+ * @param {module:main~Condition} condition A condition.
+ * @param {...*} message An error message.
+ * @return {booolean} TRUE when the condition is met, otherwise FALSE.
+ */
+function errorIf(condition, ...message) {
+  if (condition) {
+    exports.error(...message);
+  }
+
+  return !!condition;
+}
+
+/**
+ * Outputs a debug message if a condition is met.
+ *
+ * @param {module:main~Condition} condition A condition.
+ * @param {...*} message A debug message.
+ * @return {booolean} TRUE when the condition is met, otherwise FALSE.
+ */
+function debugIf(condition, ...message) {
+  if (condition) {
+    exports.debug(...message);
+  }
+
+  return !!condition;
+}
+
+/**
+ * Throws a user-defined exception if a condition is met.
+ *
+ * @example
+ * function _foo(num) {
+ *   throwIf(typeof num !== 'number', new TypeError('Argument num must be a number.'));
+ *   ...
+ * }
+ * @param {module:main~Condition} condition A condition.
+ * @param {string|number|boolean|object} expression An expression to throw
+ *        (please prefer an instance of Error, because it contains an original
+ *        location, where it was created).
+ */
+function throwIf(condition, expression) {
+  if (condition) {
+    throw expression;
+  }
+}
+
+/**
+ * Returns a rejected promise if a condition is met.
+ *
+ * @example
+ * function asyncFoo(num, str) {
+ *   let rejected;
+ *
+ *   if (
+ *     (rejected = rejectIf(typeof num !== 'number', new TypeError('Argument num must be a number.'))) ||
+ *     (rejected = rejectIf(typeof str !== 'string', new TypeError('Argument str must be a number.')))
+ *   ) {
+ *     return rejected;
+ *   }
+ *   ...
+ * }
+ * @param {module:main~Condition} condition A condition.
+ * @param {string|number|boolean|object} reason A reason of rejecting (please
+ *        prefer an instance of Error, because it contains an original location,
+ *        where it was created).
+ * @return {?Promise} A promise that is rejected with the given reason or null.
+ */
+function rejectIf(condition, reason) {
+  if (condition) {
+    return Promise.reject(reason);
+  }
+
+  return null;
+}
+
+let $registerImaPlugin = ns => {
+  ns.namespace('plugin.logger');
+};
+
+exports.$registerImaPlugin = $registerImaPlugin;
+exports.configureLogger = configureLogger;
+exports.beSilent = beSilent;
+exports.isSilent = isSilent;
+exports.log = log;
+exports.info = info;
+exports.warn = warn;
+exports.error = error;
+exports.debug = debug;
+exports.logIf = logIf;
+exports.infoIf = infoIf;
+exports.warnIf = warnIf;
+exports.errorIf = errorIf;
+exports.debugIf = debugIf;
+exports.throwIf = throwIf;
+exports.rejectIf = rejectIf;
