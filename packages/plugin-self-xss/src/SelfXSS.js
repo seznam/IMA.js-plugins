106cd549da799c2848f9b775f1864c879498419b
Jan Kučera <jan.kucera@firma.seznam.cz>
2019-12-10 15:51:11 +0100

    fix(SelfXSS) - fix dictionary keys

--- packages/plugin-self-xss/src/SelfXSS.js
+++ packages/plugin-self-xss/src/SelfXSS.js
@@ -7,11 +7,11 @@ export default class SelfXSS {
   }
 
   static get DICTIONARY_TITLE_KEY() {
-    return '@ima/plugin-self-xss.title';
+    return 'ima-plugin-self-xss.title';
   }
 
   static get DICTIONARY_PHASE_KEY() {
-    return '@ima/plugin-self-xss.phase';
+    return 'ima-plugin-self-xss.phase';
   }
 
   /**
