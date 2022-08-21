0d4f9fb54c67df187cf5fc5528b6a4951da8b1b9
Jan Šimeček <simecekjann@gmail.com>
2022-08-21 20:07:44 +0200

    Created separate entry point for less constants units

--- packages/cli-plugin-less-constants/src/main.ts
+++ packages/cli-plugin-less-constants/src/main.ts
@@ -1,5 +1,1 @@
 export { LessConstantsPlugin } from './LessConstantsPlugin';
-
-export * as units from './units/units';
-export * as media from './units/media';
-export { MapUnit, Unit, asUnit } from './units/utils';
---
+++ packages/cli-plugin-less-constants/src/units/index.ts
@@ -0,0 +1,3 @@
+export * as units from './units';
+export * as media from './media';
+export { MapUnit, Unit, asUnit } from './utils';
