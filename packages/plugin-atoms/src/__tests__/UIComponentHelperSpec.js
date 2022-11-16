75c4897b8b8a8a96938a3280c97b155a4ff39e73
Zdeněk Laštůvka <zdenek.lastuvka@firma.seznam.cz>
2022-11-16 13:03:05 +0100

    Add tests for sanitizeUrl method

--- packages/plugin-atoms/src/__tests__/UIComponentHelperSpec.js
+++ packages/plugin-atoms/src/__tests__/UIComponentHelperSpec.js
@@ -100,6 +100,54 @@ describe('UIComponentHelper', () => {
     });
   });
 
+  describe('sanitizeUrl method', () => {
+    describe('valid URLs', () => {
+      const validUrls = [
+        '',
+        'http://abc',
+        'HTTP://abc',
+        'https://abc',
+        'HTTPS://abc',
+        'ftp://abc',
+        'FTP://abc',
+        'mailto:me@example.com',
+        'MAILTO:me@example.com',
+        'tel:123-123-1234',
+        'TEL:123-123-1234',
+        '#anchor',
+        '/page1.md',
+        'http://JavaScript/my.js',
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/',
+        'data:video/webm;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/',
+        'data:audio/opus;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/',
+      ];
+      for (const url of validUrls) {
+        it(`valid ${url}`, () =>
+          expect(uiComponentHelper.sanitizeUrl(url)).toEqual(url));
+      }
+    });
+
+    describe('invalid URLs', () => {
+      const invalidUrls = [
+        'javascript:evil()',
+        'JavaScript:abc',
+        'evilNewProtocol:abc',
+        ' \n Java\n Script:abc',
+        '&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;',
+        '&#106&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;',
+        '&#106 &#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;',
+        '&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058',
+        '&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A;',
+        'jav&#x09;ascript:alert();',
+        'jav\u0000ascript:alert();',
+      ];
+      for (const url of invalidUrls) {
+        it(`valid ${url}`, () =>
+          expect(uiComponentHelper.sanitizeUrl(url)).toMatch(/^unsafe:/));
+      }
+    });
+  });
+
   describe('serializeObjectToNoScript method', () => {
     let ariaProps = {
       'aria-label': 'something',
