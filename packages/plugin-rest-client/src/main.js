65dccee1b248da3ffa5a6a1d483414baf6f396fc
Zdeněk Laštůvka <zdenek.lastuvka@firma.seznam.cz>
2023-04-12 11:12:50 +0200

    Fix export

--- packages/plugin-rest-client/src/main.js
+++ packages/plugin-rest-client/src/main.js
@@ -1,5 +1,6 @@
 export { default as AbstractResource } from './AbstractResource';
 export { PathType } from './AbstractResource';
+export { default as AbstractEntity } from './AbstractEntity';
 export { default as Processor } from './processor/Processor';
 export { Operation } from './processor/Processor';
 export { default as RestClient } from './RestClient';
