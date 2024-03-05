5687effe812595460b12da9450538f94ed530efc
Rayus7 <sommer.p7@gmail.com>
2024-03-05 14:46:18 +0100

    fix unit tests

--- packages/plugin-analytic-fb-pixel/src/__tests__/mainSpec.js
+++ packages/plugin-analytic-fb-pixel/src/__tests__/mainSpec.js
@@ -4,8 +4,4 @@ describe('Main', () => {
   it('should export FacebookPixelAnalytic', () => {
     expect(typeof Main.FacebookPixelAnalytic).toBe('function');
   });
-
-  it('should export defaultDependencies', () => {
-    expect(Array.isArray(Main.defaultDependencies)).toBe(true);
-  });
 });
--- packages/plugin-analytic-google/src/__tests__/mainSpec.js
+++ packages/plugin-analytic-google/src/__tests__/mainSpec.js
@@ -4,8 +4,4 @@ describe('Main', () => {
   it('should export GoogleAnalytics4', () => {
     expect(typeof Main.GoogleAnalytics4).toBe('function');
   });
-
-  it('should export googleAnalytics4DefaultDependencies', () => {
-    expect(Array.isArray(Main.googleAnalytics4DefaultDependencies)).toBe(true);
-  });
 });
