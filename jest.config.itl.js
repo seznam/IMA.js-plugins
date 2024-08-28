0cdfe97ab97c06c1b141d187e97cbe2dc1b63e9d
Filip Satek <filip.satek@firma.seznam.cz>
2024-08-28 17:08:34 +0200

    test: migrate to IMA Testing Library

---
+++ jest.config.itl.js
@@ -0,0 +1,20 @@
+const {
+  setImaTestingLibraryServerConfig,
+  FALLBACK_APP_MAIN_PATH,
+  FALLBACK_APPLICATION_FOLDER,
+} = require('@ima/testing-library/server');
+
+const base = require('./jest.config.base.js');
+
+setImaTestingLibraryServerConfig({
+  applicationFolder: FALLBACK_APPLICATION_FOLDER,
+});
+
+module.exports = {
+  ...base,
+  preset: '@ima/testing-library',
+  moduleNameMapper: {
+    'app/main': FALLBACK_APP_MAIN_PATH,
+  },
+  setupFiles: ['<rootDir>/../../jestSetup.itl.js'],
+};
--- packages/plugin-atoms/jest.config.js
+++ packages/plugin-atoms/jest.config.js
@@ -1,20 +1,5 @@
-const {
-  setImaTestingLibraryServerConfig,
-  FALLBACK_APP_MAIN_PATH,
-  FALLBACK_APPLICATION_FOLDER,
-} = require('@ima/testing-library/server');
-
-const base = require('../../jest.config.base.js');
-
-setImaTestingLibraryServerConfig({
-  applicationFolder: FALLBACK_APPLICATION_FOLDER,
-});
+const itl = require('../../jest.config.itl.js');
 
 module.exports = {
-  ...base,
-  preset: '@ima/testing-library',
-  moduleNameMapper: {
-    'app/main': FALLBACK_APP_MAIN_PATH,
-  },
-  setupFiles: ['./jestSetup.js'],
+  ...itl,
 };
