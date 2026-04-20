995582292accd12efb91839a9be5a829320f3ae2
Jiří Fencl <jiri.fencl@firma.seznam.cz>
2026-04-20 13:55:10 +0200

    Refactor testing-integration to rely on ima/testing-library

--- packages/plugin-testing-integration/src/__tests__/mainSpec.js
+++ packages/plugin-testing-integration/src/__tests__/mainSpec.js
@@ -1,7 +1,26 @@
+jest.mock('@ima/testing-library', () => ({
+  __esModule: true,
+  setImaTestingLibraryClientConfig: jest.fn(),
+  generateDictionary: jest.fn().mockResolvedValue({}),
+}));
+
 import * as main from '../main';
 
 describe('Main', () => {
-  it('can export all necessary properties', () => {
-    expect(main).toMatchSnapshot();
+  it('exports plugin APIs', () => {
+    expect(main).toEqual(
+      expect.objectContaining({
+        getConfig: expect.any(Function),
+        setConfig: expect.any(Function),
+        initImaApp: expect.any(Function),
+        clearImaApp: expect.any(Function),
+      })
+    );
+  });
+
+  it('does not re-export testing-library utilities', () => {
+    expect(main).not.toHaveProperty('renderWithContext');
+    expect(main).not.toHaveProperty('screen');
+    expect(main).not.toHaveProperty('setupImaTestingIntegration');
   });
 });
